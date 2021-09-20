2270a1639cc6067a6de01c63b42b504c75acc18c
saracacahuate <90066330+saracacahuate@users.noreply.github.com>
2021-09-20 15:30:33 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var hammer;
+var rubber1;
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20)
+    hammer = new Hammer(10,100);
+    rubber1 = new Rubber(20,500,50);
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+
+    plane.display();
+    hammer.display();
+    Rubber.display();
+
+    
+ 
+}
---
+++ stone.js
@@ -0,0 +1,8 @@
+class stone{
+    constructor(x,y,r){
+        var options ={resitution: 0.8,friction:0.9,density:12}
+		this.body = Bodies.rectangule(x,y,50,50,options)
+
+    }
+
+}
